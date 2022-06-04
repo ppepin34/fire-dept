21531381ec8a38f38f273df9a0c43e8467b6618e
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-06-03 22:59:07 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.JAWSDB_NAME, process.env.JAWSDB_USER, process.env.JAWSDB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
