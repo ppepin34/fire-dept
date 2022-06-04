c2cddfa3151f4f15e0f85e24b3b8a16f13cdc6bd
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-06-03 23:02:52 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.JAWSDB_NAME, process.env.JAWSDB_USER, process.env.JAWSDB_PW, {
+let sequelize;
+
+if (process.env.JAWS_URL) {
+  sequelize = new Sequelize(process.env.JAWS_URL);
+} else {
+  sequelize = new Sequelize(process.env.JAWSDB_NAME, process.env.JAWSDB_USER, process.env.JAWSDB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      port: 3306
+  });
+}
 
 module.exports = sequelize;
