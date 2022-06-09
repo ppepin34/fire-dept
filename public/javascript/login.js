2e1f725a79632f91c8db62dc52aaeb01f8a84435
youngff43 <youngff43@gmail.com>
2022-06-09 00:39:11 -0400

    added create emploee function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -21,41 +21,6 @@ async function loginFormHandler(event) {
         alert('Login attempt unsuccessful. Please check your email and password and try again.');
       }
     }
-  }
-  
-async function addEmpFormHandler(event) {
-    event.preventDefault();
+  };
 
-    const lastName = document.querySelector('#modalLN').value.trim();
-    const firstName = document.querySelector('#modalFN').value.trim();
-    const username = document.querySelector('#modalUN').value.trim();
-    const email = document.querySelector('#modalEmail').value.trim();
-    const rank = document.querySelector('#modalRank').value.trim();
-    const stationID = document.querySelector('#modalSID').value.trim();
-    const password = document.querySelector('#modalPW').value.trim();
-  
-    if (lastName && firstName && email && rank && stationID && password) {
-      const response = await fetch('/api/employee', {
-        method: 'post',
-        body: JSON.stringify({
-          lastName,
-          firstName,
-          username,
-          email,
-          rank,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      // check the response status
-      if (response.ok) {
-        console.log('success');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-document.querySelector('.add-employee').addEventListener('submit', addEmpFormHandler);
