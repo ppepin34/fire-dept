8da8af099154f43589343731f184a19c02cf89bd
ppepin34 <ppepin34@gmail.com>
2022-06-12 16:19:19 -0400

    initial js for update employee

---
+++ public/javascript/updateemployee.js
@@ -0,0 +1,25 @@
+async function updateFormHandler(event) {
+    event.preventDefault();
+
+    // const id = 
+    // const rank =
+    // const station_id =
+    // const certs = 
+    const response = await fetch(`/api/employee/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            rank,
+            station_id,
+            certIDs
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if(response.ok) {
+        document.location.replace('/info/');
+    } else {
+        alert(response.statusText);
+    }
+}
