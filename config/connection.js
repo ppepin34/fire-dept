9fd48bac010c311ef9222b092b63971b258a3f4a
Darobinson20 <Darobinson20@gmail.com>
2022-06-12 19:16:42 -0400

    corrected update modal

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ const Sequelize = require("sequelize");
 // to use contents of .env file (! Make sure they match your .env file names !)
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-<<<<<<< HEAD
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
@@ -16,20 +15,6 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
-=======
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "mysql",
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
->>>>>>> 3f8ff03ee2821f138d9d8cbf73c3fe01d95c8ed3
 
 // export to use in other files
 module.exports = sequelize;
